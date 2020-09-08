901ef9fb627cea04b150c3b546e649de19860061
张志潮 <zhangzhichao@zhangzhichaodeMacBook-Air.local>
2020-09-08 16:33:42 +0800

    config: rm .umi

--- src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/zhangzhichao/work/df-work/dffl-fuman-product/scaffold-m/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','dva','getInitialState','locale','locale','request',],
-});
-
-export { plugin };
--- src/.umi/plugin-helmet/exports.ts
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-// @ts-ignore
-export { Helmet } from '/Users/zhangzhichao/work/df-work/dffl-fuman-product/scaffold-m/node_modules/react-helmet';
