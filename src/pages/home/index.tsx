b43334eea60334a2bc003c07efb245ac957b4e8f
张志潮 <zhangzhichao@zhangzhichaodeMacBook-Air.local>
2021-01-27 11:22:51 +0800

    feat: home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -15,7 +15,43 @@ const Home: FC<PropTypes> = function (props) {
         <h3>{i18n.formatMessage({ id: 'home_banner_text' })}</h3>
         <div>{i18n.formatMessage({ id: 'home_getintouch' })}</div>
       </section>
-      <section className={styles.highcosts}></section>
+      <section className={styles.highcosts}>
+        <h2>{i18n.formatMessage({ id: 'home_costs_head' })}</h2>
+        <ul>
+          <li>
+            <h3>
+              {i18n.formatMessage({ id: 'home_costs_item_head_1_1' })}
+              <br />
+              {i18n.formatMessage({ id: 'home_costs_item_head_1_2' })}
+            </h3>
+            <p>{i18n.formatMessage({ id: 'home_costs_item_text_1' })}</p>
+          </li>
+          <li>
+            <h3>
+              {i18n.formatMessage({ id: 'home_costs_item_head_2_1' })}
+              <br />
+              {i18n.formatMessage({ id: 'home_costs_item_head_2_2' })}
+            </h3>
+            <p>{i18n.formatMessage({ id: 'home_costs_item_text_2' })}</p>
+          </li>
+          <li>
+            <h3>
+              {i18n.formatMessage({ id: 'home_costs_item_head_3_1' })}
+              <br />
+              {i18n.formatMessage({ id: 'home_costs_item_head_3_2' })}
+            </h3>
+            <p>{i18n.formatMessage({ id: 'home_costs_item_text_3' })}</p>
+          </li>
+          <li>
+            <h3>
+              {i18n.formatMessage({ id: 'home_costs_item_head_4_1' })}
+              <br />
+              {i18n.formatMessage({ id: 'home_costs_item_head_4_2' })}
+            </h3>
+            <p>{i18n.formatMessage({ id: 'home_costs_item_text_4' })}</p>
+          </li>
+        </ul>
+      </section>
       <section className={styles.values}></section>
       <section className={styles.why}></section>
       <section className={styles.advisory}></section>
