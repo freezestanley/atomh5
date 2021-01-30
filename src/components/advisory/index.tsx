f592dd8524b71a5d03b23ecabaaf5d10bbd024f0
张志潮 <zhangzhichao@zhangzhichaodeMacBook-Air.local>
2021-01-30 16:45:03 +0800

    feat: 大图

--- src/components/advisory/index.tsx
+++ src/components/advisory/index.tsx
@@ -7,6 +7,7 @@ import { useIntl } from 'umi'
 
 // const charts = [chart1, chart2, chart3, chart4, chart5, chart6];
 const imgs = [require('./images/atom8_logo_s.png')]
+const banner = [require('./images/home_banner2.png')]
 const charts = [
   require('./images/icon-chart-1.png'),
   require('./images/icon-chart-2.png'),
@@ -27,26 +28,16 @@ const Advisory: FC<PropTypes> = function (props) {
   return (
     <div className={styles['advisory']}>
       <h2>{i18n.formatMessage({ id: 'home_advisory_head' })}</h2>
-      <div>
+      <section></section>
+      <div className={styles['text']}>
         <h3>{i18n.formatMessage({ id: 'home_advisory_text_1' })}</h3>
-        <h3>{i18n.formatMessage({ id: 'home_advisory_text_2' })}</h3>
+        {/* <h3>{i18n.formatMessage({ id: 'home_advisory_text_2' })}</h3>
         <h3>{i18n.formatMessage({ id: 'home_advisory_text_3' })}</h3>
         <h3>{i18n.formatMessage({ id: 'home_advisory_text_4' })}</h3>
         <h3>{i18n.formatMessage({ id: 'home_advisory_text_5' })}</h3>
         <h3>{i18n.formatMessage({ id: 'home_advisory_text_6' })}</h3>
         <h4>{i18n.formatMessage({ id: 'home_advisory_text_7' })}</h4>
-        <h4>{i18n.formatMessage({ id: 'home_advisory_text_8' })}</h4>
-        <img src={charts[0]} />
-        <img src={charts[1]} />
-        <img src={charts[2]} />
-        <img src={charts[3]} />
-        <img src={charts[4]} />
-        <img src={charts[5]} />
-        <img src={imgs[0]} />
-        <img src={logos[0]} />
-        <img src={logos[1]} />
-        <img src={logos[2]} />
-        <img src={logos[3]} />
+        <h4>{i18n.formatMessage({ id: 'home_advisory_text_8' })}</h4> */}
       </div>
     </div>
   )
