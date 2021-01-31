f82e80c755c648c31ef69398def221ce3ca0761e
mmf <987756125@qq.com>
2021-01-31 20:32:52 +0800

    feat: footer国际化

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,8 +3,10 @@
  */
 import React, { FC } from 'react'
 import styles from './styles/index.less'
+import { useIntl } from 'umi'
 interface PropTypes {}
 const Footer: FC<PropTypes> = function (props) {
+  const i18n = useIntl();
   return (
     <>
       <div className={styles['footer-pre']}>
@@ -12,10 +14,10 @@ const Footer: FC<PropTypes> = function (props) {
           <img src={require('./images/atom8_logo_s.png')} />
         </div>
         <div className={styles['page-list']}>
-          <span>HOME</span>
-          <span>STO PROJECTS</span>
-          <span>How It Works</span>
-          <span>Who We Are</span>
+          <span>{i18n.formatMessage({ id: 'footer_home' })}</span>
+          <span>{i18n.formatMessage({ id: 'footer_stoprojects' })}</span>
+          <span>{i18n.formatMessage({ id: 'footer_howitworks' })}</span>
+          <span>{i18n.formatMessage({ id: 'footer_whoweare' })}</span>
         </div>
       </div>
       <div className={styles['footer-next']}>
@@ -23,9 +25,9 @@ const Footer: FC<PropTypes> = function (props) {
       </div>
       <footer>
         <ul>
-          <li>Data Privacy Policy</li>
+          <li>{i18n.formatMessage({ id: 'footer_stoprojects_description' })}</li>
           <li>User Agreement</li>
-          <li>Cookies Policy</li>
+          <li>{i18n.formatMessage({ id: 'footer_howitworks_description' })}</li>
         </ul>
       </footer>
     </>
